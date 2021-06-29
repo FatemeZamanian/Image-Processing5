f3b51b080016e719633382602d9d9ffed80b567c
FatemeZamanian <fatemehzamanian.1999@gmail.com>
2021-06-30 02:03:37 +0430

    up2

--- colors.py
+++ colors.py
@@ -1,48 +1,40 @@
 import cv2
 import numpy as np
+from filters import filter
+import threading
+import matplotlib.pyplot as plt
 
-def filterr(img,size,value):
-    row,col=img.shape
-    res=np.zeros((row,col),dtype='uint8')
-    mask = np.ones((size,size),dtype='uint8')*value
-    m=size//2
-    n=m+1
-    print(n,m)
-    for i in range(m, row - m):
-        for j in range(m, col - m):
-            small_image = img[i - m:i + n, j - m:j + n]
-            besco = np.multiply(small_image, mask)
-            out = np.sum(besco)
-            res[i, j] = out
-    return res
+class color_detector(threading.Thread):
+    def __init__(self):
+        super(color_detector,self).__init__()
+    def run(self):
+        self.video=cv2.VideoCapture(0)
+        while True:
+            res,frame=self.video.read()
+            if res==False:
+                break
+            row,col,ch=frame.shape
+            frame=cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+            mask = frame[row//10:row//2, col//10:col//2]
+            mask=cv2.equalizeHist(mask)
+            result=filter(frame,15,1/255)
+            result[row//10:row//2, col//10:col//2]=mask
+            rm,cm=mask.shape
+            c=0
+            for i in range(rm):
+                for j in range (cm):
+                    c+=mask[i,j]
+            c=c//(rm*cm)
+            if c<100:
+                color='black'
+            elif c>=100 and c<=170:
+                color='gray'
+            else:
+                color='white'
+            cv2.putText(result,color,(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2)
+            cv2.imshow('',result)
+            cv2.waitKey(1)
 
-
-video=cv2.VideoCapture(0)
-
-while True:
-    res,frame=video.read()
-    if res==False:
-        break
-    row,col,ch=frame.shape
-    frame=cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
-    mask = frame[row//10:row//2, col//10:col//2]
-    result=filterr(frame,15,1/255)
-    result[row//10:row//2, col//10:col//2]=mask
-    rm,cm=mask.shape
-    c=0
-    for i in range(rm):
-        for j in range (cm):
-            c+=mask[i,j]
-    c=c//(rm*cm)
-    if c<100:
-        print('black')
-    elif c>=100 and c<=200:
-        print('gray')
-    else:
-        print('white')
-
-    cv2.imshow('',result)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+theared=color_detector()
+theared.start()
 
